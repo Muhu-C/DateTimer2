26ad9898e8ebf753b21a31a98d7872a491fd2441
Muhu-C <452044217@qq.com>
2024-08-22 17:21:08 +0800

    a·

--- DateTimer.WPF/View/HomePage.xaml.cs
+++ DateTimer.WPF/View/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iNKORE.UI.WPF.Modern;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,6 +32,12 @@ namespace DateTimer.WPF.View
             mw.ContentFrame.Navigate(mw._settingsPage);
             mw.TitleText.Text = "设置";
             mw.Navigation.SelectedItem = mw.SettingButton;
+        }
+
+        private void ShowTimer_Click(object sender, RoutedEventArgs e)
+        {
+            if(ThemeManager.Current.ApplicationTheme == ApplicationTheme.Light)ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+            else ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
         }
     }
 }
